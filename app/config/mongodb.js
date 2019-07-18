547575193b02afa4f319d2dc6842dae851beef77
Saeed Ghotb <saeedghotb95@gmail.com>
2019-07-18 18:03:47 +0430

    mongodb setted up

--- app/config/mongodb.js
+++ app/config/mongodb.js
@@ -2,4 +2,11 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.promise = global.Promis;
-mongoose.connect(process.env.dbaddress);
+mongoose.connect(process.env.dbaddress,{useNewUrlParser: true});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console,'mongo db connection connection error:'))
+db.once('open',function(){
+    console.log('connected to database')
+})
